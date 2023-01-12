ee3cf9d5f96a6b4cbdbaab4a49e149f75b75daeb
ChenZheng <1007324849@qq.com>
2023-01-13 00:12:41 +0800

    feat: user and comment apis

--- src/main/java/icu/chenz/commentx/service/CommentService.java
+++ src/main/java/icu/chenz/commentx/service/CommentService.java
@@ -27,7 +27,7 @@ public class CommentService {
     private UserDao userDao;
 
     public Map<String, List<? extends Entity>> getByContext(String context) {
-        HashMap<String, List<? extends Entity>> res = new HashMap<>();
+        HashMap<String, List<? extends Entity>> res = new HashMap<>(2);
         List<CommentEntity> comments = commentDao.getByContext(context);
         res.put("comments", comments);
         res.put("users", userDao.getByIds(comments.stream().map(CommentEntity::getUser).collect(Collectors.toSet())));
