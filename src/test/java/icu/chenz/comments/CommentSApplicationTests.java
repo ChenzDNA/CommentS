8d44964d0ed92426d0686a528f3ff3072c3c802e
ChenZheng <1007324849@qq.com>
2023-02-20 22:58:42 +0800

    test

--- src/test/java/icu/chenz/comments/CommentSApplicationTests.java
+++ src/test/java/icu/chenz/comments/CommentSApplicationTests.java
@@ -1,10 +1,19 @@
 package icu.chenz.comments;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
 import icu.chenz.comments.config.AuthorConfig;
-import icu.chenz.comments.service.CommentService;
+import icu.chenz.comments.controller.CommentController;
+import icu.chenz.comments.entity.CommentEntity;
+import icu.chenz.comments.entity.IDEntity;
+import icu.chenz.comments.utils.R;
+import icu.chenz.comments.utils.exception.BadRequest;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.Map;
 
 @SpringBootTest
 class CommentSApplicationTests {
@@ -13,7 +22,7 @@ class CommentSApplicationTests {
     private AuthorConfig authorConfig;
 
     @Resource
-    private CommentService commentService;
+    private CommentController commentController;
 
     @Test
     public void s() {
@@ -21,7 +30,24 @@ class CommentSApplicationTests {
     }
 
     @Test
-    public void a() {
+    public void commentControllerCreateAndTop() throws BadRequest {
+        R<CommentEntity> r = commentController.create(1L, new CommentEntity("45666", "123"), null);
+        Assert.notNull(r.getData(), "commentController.create returns null.");
+        System.out.println(r.getData());
+
+        R<Integer> top = commentController.top(6L, new IDEntity(r.getData().getId()));
+        Assert.notNull(r.getData(), "commentController.top returns null.");
+        Assert.isTrue(top.getData() == 1, "commentController.top returns not 1.");
     }
 
+    @Test
+    public void commentControllerGets() {
+        R<Map<String, Object>> byContext = commentController.getByContext(1L, "789");
+        Assert.notNull(byContext.getData(), "commentController.getByContext returns null.");
+        System.out.println(JSONObject.toJSONString(
+                byContext.getData(),
+                JSONWriter.Feature.PrettyFormat,
+                JSONWriter.Feature.WriteNulls
+        ));
+    }
 }
